38dcb0cbb22ea31ec20c210f0fc265089ca84b5b
Thiago <thidurante@hotmail.com>
2023-03-17 01:41:40 -0300

    feat: starts swagger documentation /products on post is done

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import swaggerUi from 'swagger-ui-express';
 import app from './app';
+import swaggerDocs from './swagger.json';
 
 const PORT = 3001;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
